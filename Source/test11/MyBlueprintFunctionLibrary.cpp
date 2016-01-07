d743d19f9d03d7a80e00404604c9329b81a2729b
Greg <gbrill@infusion.com>
2016-01-07 15:58:05 -0500

    seemingly working SCALE_X and Y added.

--- Source/test11/MyBlueprintFunctionLibrary.cpp
+++ Source/test11/MyBlueprintFunctionLibrary.cpp
@@ -55,8 +55,16 @@ void UMyBlueprintFunctionLibrary::createGraph(int32 id, float height, float widt
 
 	float rangeX = FMath::Abs(maxRangeX) + FMath::Abs(minRangeX);
 	float rangeY = FMath::Abs(maxRangeY) + FMath::Abs(minRangeY);
-	fg.scaleX = width / rangeX;// scaleX;// height;// scaleX;// fg.height / FMath::Abs(maxRangeX) / 2;
-	fg.scaleY = height / rangeY;//  scaleY;// width;// scaleY; // / FMath::Abs(maxRangeY) / 2;
+	
+//	fg.scaleX = width / rangeX;// scaleX;// height;// scaleX;// fg.height / FMath::Abs(maxRangeX) / 2;
+//	fg.scaleY = height / rangeY;//  scaleY;// width;// scaleY; // / FMath::Abs(maxRangeY) / 2;
+		
+	
+	fg.scaleX = scaleX;// scaleX;// height;// scaleX;// fg.height / FMath::Abs(maxRangeX) / 2;
+	fg.scaleY = scaleY;// / rangeY;//  scaleY;// width;// scaleY; // / FMath::Abs(maxRangeY) / 2;
+
+
+	
 	fg.rangeOffsetX =  minRangeX;
 	fg.rangeOffsetY =  minRangeY;
 
@@ -387,10 +395,24 @@ void UMyBlueprintFunctionLibrary::generateGraphOffsetsFromID(int32 id, FVector4 
 #else
 
 
+	
 
-	float ylevel = ((fg.height - line.Y)/fg.scaleY  - fg.maxRangeY );
+
+	///(FMath::Abs(fg.maxRangeY) + FMath::Abs(fg.minRangeY)) - fg.maxRangeY )
+	//float ylevel = ((fg.height - line.Y)/fg.scaleY  - fg.maxRangeY );//TODO:divide by scale
+//	float ylevel = ((fg.height - line.Y) / (float)fg.cellHeightY - fg.maxRangeY);// +FMath::Abs(fg.maxRangeY - fg.minRangeY) / fg.scaleY;//TODO:divide by scale
+	
+	/*
+	def test(x, minrange) :
+		val = x*scale + minrange
+		return val*/
+	
+	float ylevel = (line.Y / fg.cellHeightY)*fg.scaleY + fg.minRangeY;
+	
 	float xlevel = line.X/fg.cellLengthX + fg.minRangeX ;
 
+
+	FString s;
 #endif
 
 	ylevel= roundf(ylevel * 100) / 100;
@@ -414,8 +436,11 @@ void UMyBlueprintFunctionLibrary::generateGraphOffsetsFromID(int32 id, FVector4 
 	}
 	else //Y Label
 	{
+		//UE_LOG(LogTemp, Warning, TEXT("y:%f h:%f mrangy:%f scale:%f range:%f calc:%f"), line.Y, fg.cellHeightY, fg.maxRangeY, fg.scaleY, FMath::Abs(fg.maxRangeY - fg.minRangeY), ylevel);
+		
+		
 		//float numHlines = fg.height / fg.cellsize;
-		labelY = FString::SanitizeFloat(ylevel);
+		labelY = FString::SanitizeFloat(ylevel);//FString::Printf(TEXT("%f"), ylevel); 
 		//	GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Red, FString::FromInt(line.Y));
 		//	labelY = FString::FromInt((int32)((line.Y)));
 		isLabelX = false;
@@ -440,10 +465,10 @@ void UMyBlueprintFunctionLibrary::generateGraphOffsetsFromID(int32 id, FVector4 
 
 
 	//is this the LL corner of Y
-	if (ylevel == fg.minRangeY && !isLabelX)
+	if (ylevel == fg.minRangeY && !isLabelX) //upper is getting this also, TODO:fix
 	{
-		cornerYOffset = 0;
-		cornerXOffset = -25 ;
+		cornerYOffset = 0;// 10;
+		cornerXOffset = 0;//  -25;
 		//just get rig of the label
 	//	labelY = "";
 	}
@@ -470,6 +495,106 @@ void UMyBlueprintFunctionLibrary::generateGraphOffsetsFromID(int32 id, FVector4 
 	//generateGraphOffsets(fg.height, fg.width, fg.cellsize, fg.offsetX, fg.offsetY, fg.scaleX, fg.scaleY, line, PointA, PointB, label);
 }
 
+//UNUSED
+UFUNCTION(BlueprintCallable, Category = "ROS_GRAPH")
+void UMyBlueprintFunctionLibrary::generateGraphOffsetsFromID2(int32 id, FVector4 line, FVector2D &PointA, FVector2D &PointB, FString & labelX, FString &labelY, bool & isLabelX, FVector2D & labelPosition, bool & isOriginLine)
+{
+	FGraphData fg;
+
+	fg = gGraphs[id];
+
+	PointA.X = fg.offsetX + line.X;
+	PointA.Y = fg.offsetY + line.Y;
+	PointB.X = fg.offsetX + line.Z;
+	PointB.Y = fg.offsetY + line.W;
+
+	isOriginLine = false;
+
+	//below just indicates the boundary of graph
+	//if ((line.X == 0 && line.W == 0) || (line.Y == 0 && line.Z == 0))
+	//	isOriginLine = true;
+
+	//UE_LOG(LogTemp, Warning, TEXT("START OFFSETS"));
+
+
+	float ylevel = ((fg.height - line.Y) / fg.scaleY) + fg.rangeOffsetY;
+	float xlevel = ((line.X) / fg.scaleX) + fg.rangeOffsetX;
+
+	ylevel = roundf(ylevel * 100) / 100;
+	xlevel = roundf(xlevel * 100) / 100;
+	//GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::White, FString::Fromf());
+	//if the position 
+
+	//X Y Z W = X Y Z=X2 W=Y2
+	if (line.Y == 0 && line.Z != 0 || line.X == 0 && line.W == fg.height) //X label
+	{
+		labelX = FString::SanitizeFloat(xlevel);
+		isLabelX = true;
+		/*	labelPosition.X = PointB.X;
+		labelPosition.Y = PointB.Y + fg.offsetLabelX*fg.cellsize;*/
+		if (labelX == "0")
+			isOriginLine = true;
+
+
+		//		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::White, FString::FromInt(line.X));
+	}
+	else //Y Label
+	{
+		float numHlines = fg.height / fg.cellsize;
+		labelY = FString::SanitizeFloat(ylevel);
+		//	GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Red, FString::FromInt(line.Y));
+		//	labelY = FString::FromInt((int32)((line.Y)));
+		isLabelX = false;
+		if (labelY == "0")
+			isOriginLine = true;
+
+
+	}
+
+#ifdef LOGIT
+	//if (ylevel == 40 )
+	//{
+	FString f = FString::Printf(TEXT("TYPE:%s, xlev:%d ylev:%d posx:%f posy:%f"), isLabelX ? TEXT("XLABEL") : TEXT("YLABEL"), xlevel, ylevel, labelPosition.X, labelPosition.Y);
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *f);
+	//}
+#endif
+
+	//to prevent overlapp at the LL corner of text
+	float cornerYOffset = 0;
+	float cornerXOffset = 0;
+
+
+
+	//is this the LL corner of Y
+	if (ylevel == fg.minRangeY && !isLabelX)
+	{
+		cornerYOffset = 0;
+		cornerXOffset = -25;
+		//just get rig of the label
+		//	labelY = "";
+	}
+
+	//if(line.X==line.Y==0 && isLabelX) 
+	//{
+	//	//GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Red, FString::FromInt(xlevel));
+	////	cornerYOffset = 0;
+	////	cornerXOffset = -20 -20;
+	//	labelX.AppendChar(U'*');
+
+	//}
+	//	
+
+
+	labelPosition.Y = PointB.Y + fg.offsetLabelY*fg.cellsize + cornerYOffset;
+	labelPosition.X = PointB.X + fg.offsetLabelX*fg.cellsize + cornerXOffset;
+
+
+	//generateGraphOffsets(fg.height, fg.width, fg.cellsize, fg.offsetX, fg.offsetY, fg.scaleX, fg.scaleY, line, PointA, PointB, label);
+}
+
+
+
+
 UFUNCTION(BlueprintCallable, Category = "ROS_GRAPH")
 bool UMyBlueprintFunctionLibrary::translateGraphPoint(int32 id, float markerSize, FVector2D PointIn, FVector2D &PointOut)
 {
@@ -477,28 +602,40 @@ bool UMyBlueprintFunctionLibrary::translateGraphPoint(int32 id, float markerSize
 	FGraphData * pFg = &gGraphs[id];
 
 	//below, windowed offset
-	float woffsetX = FMath::Abs(pFg->maxRangeX - pFg->minRangeX)*pFg->cellLengthX;
-	float woffsetY = FMath::Abs(pFg->maxRangeY - pFg->minRangeY)* pFg->cellHeightY;
+	//float woffsetX = FMath::Abs(pFg->maxRangeX - pFg->minRangeX)*pFg->cellLengthX;
+	//float woffsetY = FMath::Abs(pFg->maxRangeY - pFg->minRangeY)* pFg->cellHeightY;
 
 	//UE_LOG(LogTemp, Warning, TEXT("X:%f < minrangeX:%f, X:%f > maxrangeX:%f, Y:%f < minrangeY:%f, Y:%f > maxrangeY:%f"), PointIn.X, pFg->minRangeX, PointIn.X, pFg->maxRangeX, PointIn.Y, pFg->maxRangeY, PointIn.Y, pFg->minRangeY);
 
 
 	//get rid of anything not viewable, ie outside the graph area
-	if (PointIn.X < pFg->minRangeX || (PointIn.X> pFg->maxRangeX) || (PointIn.Y > pFg->maxRangeY) || (PointIn.Y < pFg->minRangeY))
-	{
-	//	GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Red, FString("Out of bounds"));
-		//return false;
-	}
-
 
 
 	//4.7
 	//height doesn't need multiplier since it is already in screen units.
-	PointOut.X = (PointIn.X)*pFg->cellLengthX + pFg->offsetX*pFg->cellLengthX - pFg->cellLengthX*pFg->minRangeX; 
-	PointOut.Y = pFg->height + pFg->minRangeY*pFg->cellHeightY - PointIn.Y*pFg->cellHeightY;// -pFg->cellHeightY*pFg->minRangeY;// +pFg->offsetY*pFg->cellHeightY;
+	PointOut.X = (PointIn.X)*pFg->cellLengthX / pFg->scaleX + pFg->offsetX*pFg->cellLengthX - pFg->cellLengthX*pFg->minRangeX/pFg->scaleX;
+	PointOut.Y = (pFg->height + pFg->minRangeY*pFg->cellHeightY/pFg->scaleY) - PointIn.Y*pFg->cellHeightY/pFg->scaleY;
+	
+	
+	
+	
+	// -pFg->cellHeightY*pFg->minRangeY;// +pFg->offsetY*pFg->cellHeightY;
 
-//	UE_LOG(LogTemp, Warning, TEXT("INX:%f INY:%f OUTX:%f OUTY:%f"), PointIn.X, PointIn.Y, PointOut.X, PointOut.Y);
-	return true;
+
+	if (!(PointIn.X < pFg->minRangeX || (PointIn.X> pFg->maxRangeX) || (PointIn.Y > pFg->maxRangeY) || (PointIn.Y < pFg->minRangeY)))
+	{
+		//	GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Red, FString("Out of bounds"));
+
+
+
+
+	//	UE_LOG(LogTemp, Warning, TEXT("INX:%f INY:%f OUTX:%f OUTY:%f"), PointIn.X, PointIn.Y, PointOut.X, PointOut.Y);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
 	//MOTHBALL all the below
 	/* 
@@ -651,7 +788,7 @@ void UMyBlueprintFunctionLibrary::recalcGraphTranslatedPoints(int32 handle)
 
 UFUNCTION(BlueprintCallable, Category = "ROS_GRAPH")
 void UMyBlueprintFunctionLibrary::returnPointSetTranslatedPoints(int32 graphHandle, int32 pointSetHandle, TArray<FVector2D> &translatedPoints, FVector2D &markerSize,
-	bool & isOverFlowUp, bool & isOverFlowDown, bool& isOverFlowLeft, bool & isOverFlowRight) //TEnumAsByte<EOverflowDirection> & overflowDirection)
+	bool & isOverFlowUp, bool & isOverFlowDown, bool& isOverFlowLeft, bool & isOverFlowRight)
 {
 	FVector2D pout;
 	//EOverflowDirection ev = EOverflowDirection::NONE;
@@ -664,6 +801,8 @@ void UMyBlueprintFunctionLibrary::returnPointSetTranslatedPoints(int32 graphHand
 	bool down = false;
 	bool left = false;
 	bool right = false;
+	bool stuffLastPoint = false;
+	FVector2D lastPoint;
 	//			overflowDirection.Empty();
 		/*	LogTemp:Warning: X:20.200005 maxRange : 20.000000
 				LogTemp : Warning : X : 27.200031 maxRange : 25.000000
@@ -691,27 +830,7 @@ void UMyBlueprintFunctionLibrary::returnPointSetTranslatedPoints(int32 graphHand
 			isOverFlowRight = true;
 			//overflowDirection = EOverflowDirection::RIGHT;
 			right = true;
-			//	pFg->maxRangeX += 5;
-				//pFg->minRangeX += 5;
 
-				/*createGraph(graphHandle,
-					pFg->height,
-					pFg->width,
-					pFg->cellsize,
-					pFg->offsetX,
-					pFg->offsetY,
-					pFg->rangeOffsetX,
-					pFg->rangeOffsetY,
-					pFg->offsetLabelX,
-					pFg->offsetLabelY,
-					pFg->defaultMarkerSize,
-					pFg->minRangeX,
-					pFg->maxRangeX,
-					pFg->minRangeX,
-					pFg->maxRangeY);
-*/
-
-//				pFg->rangeOffsetX + 5;
 
 
 		}
@@ -730,14 +849,31 @@ void UMyBlueprintFunctionLibrary::returnPointSetTranslatedPoints(int32 graphHand
 			down = true;
 		}
 
+		//dont bother to process a point that isn't in view/frame
+		//if (up || down || left ||right )
+		//{
+		//	//return;
+		//}
+
+
 		//problem is that in line drawing mode, it will act as though it didn't drop out and will connect with next in-frame point
 		if (translateGraphPoint(graphHandle, gGraphs[graphHandle].defaultMarkerSize, p, pout))
 		{
+
+	//		stuffLastPoint = false;
 			translatedPoints.Add(pout);
+		//	opacity = 1;
 		}
-		else
-		{
+		else //point is not in range
+		{//stuff the last 
+			//if first encountring out of bounds point, save it and set flag
+		//	if(!stuffLastPoint)
+			//	lastPoint = pout;
+			
+		//	stuffLastPoint = true;
+
 			translatedPoints.Add(pout);
+		//	opacity = 0;
 		}
 
 
@@ -764,8 +900,8 @@ bool UMyBlueprintFunctionLibrary::generateAxisFromID2(int32 id, TArray<FVector4>
 //	float Xrange = FMath::Abs(pFg->maxRangeX) + FMath::Abs(pFg->minRangeX);
 
 //xrange and yrange are the number of ticks or lines for the axis
-	float Xrange = FMath::Abs(pFg->maxRangeX - pFg->minRangeX);
-	float Yrange = FMath::Abs(pFg->maxRangeY - pFg->minRangeY); //TODO: figure out the + - issue
+	float Xrange = FMath::Abs(pFg->maxRangeX - pFg->minRangeX)/pFg->scaleX;
+	float Yrange = FMath::Abs(pFg->maxRangeY - pFg->minRangeY)/pFg->scaleY;
 
 
 	//assume 10 -0 = 10
@@ -777,7 +913,7 @@ bool UMyBlueprintFunctionLibrary::generateAxisFromID2(int32 id, TArray<FVector4>
 
 	//fg.scaleX = (float)nVXlines / fg.width / (float)fg.cellsize; // ||
 	int nHYlines = Yrange;
-	pFg->cellHeightY = (int)(pFg->height / (float)nHYlines);// = //TEMPORARY!
+	pFg->cellHeightY = (int)(pFg->height / (float)nHYlines)  ;// = 
 
 	//UE_LOG(LogTemp, Warning, TEXT("nvx:%d nhy:%d xrange:%f yrange:%f, cellLengthX:%d, cellLengthY:%f, width:%f, height:%f"), nVXlines, nHYlines,
 //		Xrange, Yrange, pFg->cellLengthX, pFg->cellHeightY, pFg->width, pFg->height);
